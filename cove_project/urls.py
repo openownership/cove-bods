0f6c792a21d46743e4be8e3c563a0e35cfc8abee
James Baster <james.baster@opendataservices.coop>
2018-11-27 10:27:10 +0000

    Simplify URLs.
    We don't need the complexity of serving everthing under a sub directory.
    
    Also, we were duplicating URL's which was generating warnings:
    
        ?: (urls.W005) URL namespace 'admin' isn't unique. You may not be able to reverse all URLs in this namespace

--- cove_project/urls.py
+++ cove_project/urls.py
@@ -1,19 +1,9 @@
-from django.conf.urls import url, include
-from django.views.generic import RedirectView
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
-from cove.urls import urlpatterns as urlpatterns_core
-
+from cove.urls import urlpatterns
 import cove_bods.views
 
-
-# Serve the OCDS validator at /validator/
-urlpatterns_core += [url(r'^data/(.+)$', cove_bods.views.explore_bods, name='explore')]
-
-urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='review/', permanent=False)),
-    url('^validator/', include(urlpatterns_core)),
-    url('^review/', include(urlpatterns_core)),
-]
+urlpatterns += [url(r'^data/(.+)$', cove_bods.views.explore_bods, name='explore')]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
