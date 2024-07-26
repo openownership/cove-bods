1d5a6aed5c3d91a93b904e6434c670f628514cb0
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-07-26 15:52:28 +0100

    requirements: Update

--- cove_project/urls.py
+++ cove_project/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
 from libcoveweb2.urls import urlpatterns
@@ -6,7 +5,7 @@ import cove_bods.views
 from django.urls import re_path
 
 urlpatterns += [re_path(r"^$", cove_bods.views.NewInput.as_view(), name="index")]
-urlpatterns += [url(r'^data/(.+)$', cove_bods.views.ExploreBODSView.as_view(), name='explore')]
+urlpatterns += [re_path(r'^data/(.+)$', cove_bods.views.ExploreBODSView.as_view(), name='explore')]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
